91f747e6e6724dc06f70aae17f9627c8aaa2c9e4
nathi27 <nathaliecham04@gmail.com>
2024-07-27 21:08:44 -0600

    Add files via upload

---
+++ InventarioDeTienda/src/main/java/com/mycompany/inventariodetienda/InventarioDeTienda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventariodetienda;
+
+/**
+ *
+ * @author Nathalie
+ */
+public class InventarioDeTienda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ InventarioDeTienda/src/main/java/com/mycompany/inventariodetienda/Usuario.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.inventariodetienda;
+
+/**
+ *
+ * @author Nathalie
+ */
+public class Usuario {
+    private String mNombre;
+    private String mPass;
+    private String mCorreoElectronico;
+
+    /**
+     * @return the mNombre
+     */
+    public String getNombre() {
+        return mNombre;
+    }
+
+    /**
+     * @param mNombre the mNombre to set
+     */
+    public void setNombre(String mNombre) {
+        this.mNombre = mNombre;
+    }
+
+    /**
+     * @return the mPass
+     */
+    public String getPass() {
+        return mPass;
+    }
+
+    /**
+     * @param mPass the mPass to set
+     */
+    public void setPass(String mPass) {
+        this.mPass = mPass;
+    }
+
+    /**
+     * @return the mCorreoElectronico
+     */
+    public String getCorreoElectronico() {
+        return mCorreoElectronico;
+    }
+
+    /**
+     * @param mCorreoElectronico the mCorreoElectronico to set
+     */
+    public void setCorreoElectronico(String mCorreoElectronico) {
+        this.mCorreoElectronico = mCorreoElectronico;
+    }
+
+  
+    
+}
