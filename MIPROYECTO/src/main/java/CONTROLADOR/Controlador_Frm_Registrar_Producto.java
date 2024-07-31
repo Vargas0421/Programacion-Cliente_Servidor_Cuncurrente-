4b90c3d3e553603e4bb17bafe131566ffc9cb698
nathi27 <nathaliecham04@gmail.com>
2024-07-30 22:58:19 -0600

    Add files via upload

---
+++ MIPROYECTO/src/main/java/CONTROLADOR/Controlador_Frm_Registrar_Producto.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CONTROLADOR;
+
+import VISTA.frm_RegistrarProducto;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Nathalie
+ */
+public class Controlador_Frm_Registrar_Producto implements ActionListener{
+    frm_RegistrarProducto frm_rp;
+
+    public Controlador_Frm_Registrar_Producto(frm_RegistrarProducto frm_rp) {
+        this.frm_rp = frm_rp;
+        this.frm_rp
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
